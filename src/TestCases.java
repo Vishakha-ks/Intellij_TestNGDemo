1638fb74188c3f8c823502fec5d6811beb4b0461
Vishakha Kumari Singh <vishakha2singh4@gmail.com>
2023-07-20 12:35:00 +0530

    My first commit

---
+++ src/TestCases.java
@@ -0,0 +1,16 @@
+import org.testng.annotations.Test;
+
+public class TestCases {
+    @Test(priority = 1)
+    void setup(){
+        System.out.println("I am inside setup");
+    }
+    @Test(priority = 2)
+    void testSteps(){
+        System.out.println("I am inside testSteps");
+    }
+    @Test(priority = 3)
+    void tearSteps(){
+        System.out.println("I am inside tearSteps");
+    }
+}
